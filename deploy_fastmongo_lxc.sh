5677212ff94d73a9fe9b55200fb311082183b0cc
Adan Ernesto Vela <adan.vela@ucf.edu>
2026-05-14 21:59:23 -0400

    mapsagain

--- deploy_fastmongo_lxc.sh
+++ deploy_fastmongo_lxc.sh
@@ -27,7 +27,7 @@ APP_GROUP="fastmongo"
 SOURCE_STAGING_DIR=""
 MONGOD_SERVICE=""
 INSTALL_LOG="/tmp/fastmongo-install.log"
-VERSION_NAME="chips"
+VERSION_NAME="maps"
 
 MONGO_DB_NAME="${MONGO_DB_NAME:-fastmongo}"
 MONGO_COLLECTION="${MONGO_COLLECTION:-app}"
